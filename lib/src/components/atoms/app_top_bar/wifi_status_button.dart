a2f181a176af77f56f4a344c03b71bcd35aa309a
rodrigoberino <r9.berino@gmail.com>
2026-02-16 17:41:23 -0300

    Add - app top bar component

--- lib/src/components/atoms/app_top_bar/wifi_status_button.dart
+++ lib/src/components/atoms/app_top_bar/wifi_status_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../tooltip/tooltip_bubble.dart';
 
 class WifiStatusButton extends StatefulWidget {
   final bool isOnline;
@@ -15,72 +16,53 @@ class WifiStatusButton extends StatefulWidget {
 }
 
 class _WifiStatusButtonState extends State<WifiStatusButton> {
-  OverlayEntry? _overlayEntry;
+  bool _showTooltip = false;
 
   void _toggleTooltip() {
-    if (_overlayEntry == null) {
-      _showTooltip();
-    } else {
-      _removeTooltip();
-    }
-  }
-}
-
- void _showTooltip() {
-    final overlay = Overlay.of(context);
-    final renderBox = context.findRenderObject() as RenderBox;
-    final position = renderBox.localToGlobal(Offset.zero);
-
-    _overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: position.dy + 60,
-        left: position.dx - 100,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: const Color(0xFFFFD6D6),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Text(
-              widget.message,
-              style: const TextStyle(color: Colors.red),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    overlay.insert(_overlayEntry!);
-  }
-
-  void _removeTooltip() {
-    _overlayEntry?.remove();
-    _overlayEntry = null;
+    setState(() {
+      _showTooltip = !_showTooltip;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _toggleTooltip,
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          color: Color(0xFFFFE0E0),
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.topRight,
+      children: [
+        //icon e bg
+        GestureDetector(
+          onTap: _toggleTooltip,
+
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xFFFFE0E0),
+            ),
+
+            child: Icon(
+              widget.isOnline ? Icons.wifi : Icons.wifi_off,
+              color: widget.isOnline ? Colors.green : Colors.red,
+            ),
+          ),
         ),
-        child: Icon(
-          widget.isOnline ? Icons.wifi : Icons.wifi_off,
-          color: widget.isOnline ? Colors.green : Colors.red,
-        ),
-      ),
+
+        //
+        if (_showTooltip)
+          Positioned(
+            top: 60,
+            right: 0,
+            child: TooltipBubble(message: widget.message),
+          ),
+      ],
     );
   }
-
-  @override
-  void dispose() {
-    _removeTooltip();
-    super.dispose();
-  }
 }
+
+/* 
+void _showTooltip() {
+  final overlay = Overlay.of(context);
+  final renderBox = context.findRender.obje ;
+  final position;
+}. */
--- lib/src/components/atoms/tooltip/tooltip_bubble.dart
+++ lib/src/components/atoms/tooltip/tooltip_bubble.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frota/src/components/atoms/tooltip/triangle_painter.dart';
+//import 'package:frota/src/components/atoms/tooltip/triangle_painter.dart';
 
 class TooltipBubble extends StatelessWidget {
   final String message;
@@ -8,18 +8,19 @@ class TooltipBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          decoration: BoxDecoration(
-            color: const Color(0xFFFFD6D6),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Text(message, style: const TextStyle(color: Colors.red)),
+    return Material(
+      elevation: 4,
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFD6D6),
+          borderRadius: BorderRadius.circular(8),
         ),
-        const TrianglePainterWidget(),
-      ],
+        child: Text(message, style: const TextStyle(color: Colors.red)),
+      ),
+
+      //const TrianglePainterWidget(),
     );
   }
 }
